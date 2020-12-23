021895895dde54506f69ec55b584e39fc48dafaa
matheusr42 <matheus.araujo.r42@gmail.com>
2020-12-22 23:41:15 -0300

    add jest

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,11 @@
+import greeter from '../index'
+
+test('it should work with default arguments', () => {
+  const resOne = greeter()
+  expect(resOne).toEqual('Hello User!')
+})
+
+test('it should work with name argument', () => {
+  const resOne = greeter('Matheus')
+  expect(resOne).toEqual('Hello Matheus!')
+})
