e76a1f9c59fdd5d134a24b97f2c9dec35374d91a
matheusr42 <matheus.araujo.r42@gmail.com>
2020-12-23 23:09:47 -0300

    test test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import sum from './sum'
+import concat from './concat'
 
 const greeter = (name = 'User') => {
   return `Hello ${name}!`
 }
 
+console.log(sum(1, 2) + concat('1', '2'))
+
 export {
   sum,
+  concat,
   greeter
 }
